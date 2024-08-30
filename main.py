a103b8249427bf0674738dde44d770803acaaf3d
Prajwal Acharya <85928261+meprajwal@users.noreply.github.com>
2024-08-31 01:57:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sys
+from flask import Flask
+import threading
 
 from src.core import main
 from src.__init__ import _banner, log, mrh
+import os
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+def run_flask_app():
+    port = int(os.environ.get('PORT', 4000))
+    app.run(host='0.0.0.0', port=port)
+
+def run_bot():
     while True:
         try:
             _banner()
@@ -12,3 +25,13 @@ if __name__ == "__main__":
             print()
             log(mrh + f"Successfully logged out of the bot\n")
             sys.exit()
+
+if __name__ == "__main__":
+    flask_thread = threading.Thread(target=run_flask_app)
+    bot_thread = threading.Thread(target=run_bot)
+    
+    flask_thread.start()
+    bot_thread.start()
+
+    flask_thread.join()
+    bot_thread.join()
